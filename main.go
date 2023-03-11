20af5e6cd78746676c735f1dd81cd8f4eb1f20ad
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-03-11 17:54:50 -0300

    Buscando todos os registros

--- main.go
+++ main.go
@@ -46,7 +46,16 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Produto: %v. Preço: R$ %.2f.", p.Name, p.Price)
+	fmt.Printf("Produto: %v. Preço: R$ %.2f.\n", p.Name, p.Price)
+	// Select All
+	products, err := selectAllProducts(db)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Lista de Produtos")
+	for _, p := range products {
+		fmt.Printf("Produto: %v. Preço: R$ %.2f.\n", p.Name, p.Price)
+	}
 }
 
 func insertProduct(db *sql.DB, product Product) error {
@@ -87,4 +96,22 @@ func selectProduct(db *sql.DB, id string) (*Product, error) {
 		return nil, err
 	}
 	return &p, nil
+}
+
+func selectAllProducts(db *sql.DB) ([]Product, error) {
+	rows, err := db.Query("select id, name, price from products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var products []Product
+	for rows.Next() {
+		var p Product
+		err = rows.Scan(&p.ID, &p.Name, &p.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+	return products, nil
 }
