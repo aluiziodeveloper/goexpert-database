dcf4a895d8c6b438997f99271b7e05ecdbf8d9c8
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-03-11 17:34:05 -0300

    Buscando um registro - QueryRow

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
@@ -40,6 +41,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	// Select One
+	p, err := selectProduct(db, product.ID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Produto: %v. Preço: R$ %.2f.", p.Name, p.Price)
 }
 
 func insertProduct(db *sql.DB, product Product) error {
@@ -66,4 +73,18 @@ func updateProduct(db *sql.DB, product *Product) error {
 		return err
 	}
 	return nil
+}
+
+func selectProduct(db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("select id, name, price from products where id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	var p Product
+	err = stmt.QueryRow(id).Scan(&p.ID, &p.Name, &p.Price)
+	if err != nil {
+		return nil, err
+	}
+	return &p, nil
 }
