6c79e8e412b479db7596b105ec92290dff88e29a
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-03-12 12:29:34 -0300

    GORM: created_at, updated_at e deleted_at (soft delete)

--- gorm/main.go
+++ gorm/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -11,9 +9,10 @@ type Sale struct {
 	ID          int `gorm:"primaryKey"`
 	Description string
 	Price       float64
+	gorm.Model
 }
 
-const dsn string = "root:root@tcp(localhost:3306)/goexpert"
+const dsn string = "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
 
 var db *gorm.DB
 var err error
@@ -31,65 +30,74 @@ func main() {
 		Price:       1200,
 	})
 
-	// Create Many
-	sales := []Sale{
-		{Description: "Notebook dell", Price: 3929.90},
-		{Description: "MacBook Pro", Price: 9129.90},
-		{Description: "Monitor", Price: 829.90},
-		{Description: "Keyboard", Price: 29.90},
-		{Description: "Mouse", Price: 29.90},
-	}
-	createMany(&sales)
-
-	// Select One
-	var sale Sale
-	findByDescription(&sale, "Monitor")
-	fmt.Println("Select One:")
-	fmt.Println(sale)
-
-	// Select All
-	var allSales []Sale
-	findAll(&allSales)
-	fmt.Println("Select All:")
-	for _, sale := range allSales {
-		fmt.Println(sale)
-	}
-
-	// Select, Limit, Offset
-	paginate(&allSales, 2, 2)
-	fmt.Println("Select Limit 2 Offset 2:")
-	for _, sale := range allSales {
-		fmt.Println(sale)
-	}
-
-	// Select, Where
-	db.Where("price > ?", 500).Find(&allSales)
-	fmt.Println("Select Where price > 500:")
-
-	for _, sale := range allSales {
-		fmt.Println(sale)
-	}
-
-	// Select, Where, Like
-	db.Where("description LIKE ?", "%Mouse%").Find(&allSales)
-	fmt.Println("Select Where description LIKE %Mouse%:")
-
-	for _, sale := range allSales {
-		fmt.Println(sale)
-	}
-
 	// Update
 	var s Sale
-	ID := 2
+	ID := 1
 	updateDescription(&s, ID, "New description")
-	db.First(&s, ID)
-	fmt.Println("Updated:")
-	fmt.Println(s)
 
 	// Delete
 	var s2 Sale
-	ID = 3
 	remove(&s2, ID)
+
+	// // Create Many
+	// sales := []Sale{
+	// 	{Description: "Notebook dell", Price: 3929.90},
+	// 	{Description: "MacBook Pro", Price: 9129.90},
+	// 	{Description: "Monitor", Price: 829.90},
+	// 	{Description: "Keyboard", Price: 29.90},
+	// 	{Description: "Mouse", Price: 29.90},
+	// }
+	// createMany(&sales)
+
+	// // Select One
+	// var sale Sale
+	// findByDescription(&sale, "Monitor")
+	// fmt.Println("Select One:")
+	// fmt.Println(sale)
+
+	// // Select All
+	// var allSales []Sale
+	// findAll(&allSales)
+	// fmt.Println("Select All:")
+	// for _, sale := range allSales {
+	// 	fmt.Println(sale)
+	// }
+
+	// // Select, Limit, Offset
+	// paginate(&allSales, 2, 2)
+	// fmt.Println("Select Limit 2 Offset 2:")
+	// for _, sale := range allSales {
+	// 	fmt.Println(sale)
+	// }
+
+	// // Select, Where
+	// db.Where("price > ?", 500).Find(&allSales)
+	// fmt.Println("Select Where price > 500:")
+
+	// for _, sale := range allSales {
+	// 	fmt.Println(sale)
+	// }
+
+	// // Select, Where, Like
+	// db.Where("description LIKE ?", "%Mouse%").Find(&allSales)
+	// fmt.Println("Select Where description LIKE %Mouse%:")
+
+	// for _, sale := range allSales {
+	// 	fmt.Println(sale)
+	// }
+
+	// // Update
+	// var s Sale
+	// ID := 2
+	// updateDescription(&s, ID, "New description")
+	// db.First(&s, ID)
+	// fmt.Println("Updated:")
+	// fmt.Println(s)
+
+	// // Delete
+	// var s2 Sale
+	// ID = 3
+	// remove(&s2, ID)
 }
 
 func create(sale *Sale) {
